2e00ee5a158c7b2f6bcfbc864be1ca4b235774e7
zhanghan <zhanhan321@126.com>
2020-10-24 18:54:16 +0800

    add toyBrower

---
+++ toyBrower/src/server.js
@@ -0,0 +1,48 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+    let body = [];
+    req.on("error", (err) => {
+        console.error(err)
+    }).on("data", (chunk) => {
+        body.push(chunk.toString());
+    }).on('end', () => {
+        // body = Buffer.concat(body).toString();
+        // console.log("body:", body);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end( `<html maaa=a >
+		<head>
+		  <style>
+			#container{
+			  width:500px;
+			  height:300px;
+			  display:flex;
+			  background-color:rgb(255,255,255);
+			}
+			#container #myid{
+				width:200px;
+				height:100px;
+				background-color:rgb(255,0,0)
+			}
+			#container .c1{
+				flex:1;
+				background-color:rgb(0,255,0)
+			}
+			#container .c2#myid.c3{
+				width:200px;
+				height:100px;
+				background-color:rgb(0,0,255)
+			}
+		</style>
+		</head>
+		<body>
+		  <div id="container">
+		    <div id="myid" class="c2 c3"></div>
+		    <div class="c1"></div>
+		  </div>
+		</body>
+    </html>`)
+    })
+}).listen(3000, () => {
+    console.log("server started")
+})
